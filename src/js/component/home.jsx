8f83a6373f53334b89844831bbd30cd2c396af7b
jossvo <jossvo26@outlook.com>
2022-12-14 19:05:24 +0000

    Final version of Landing Page

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import NavBar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Cards from "./cards.jsx";
+import Footer from "./footer.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,20 +10,15 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<NavBar/>
+			<div className="container">
+				<Jumbotron/>
+				<Cards/>
+			</div>
+			<Footer/>
 		</div>
+		
 	);
 };
 
